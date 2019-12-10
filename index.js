a431c25a034388b568a366cac2a60fb288e296d0
Tomás Muñoz Testón <tomini18@hotmail.com>
2019-12-10 20:11:31 +0100

    creado "mutador" y base de datos fake.

--- index.js
+++ index.js
@@ -9,19 +9,37 @@ app.get('/', (req, res) => {
     res.send('todo listo');
 });
 
-// el resolver
-const root = {cliente: () => {
-    return {
-        "id": 123123123123,
-        "nombre": "Tomy",
-        "apellido": "Munoz",
-        "empresa": "Empresa",
-        "emails": [
-            {email: "correo@hotmail.com"},
-            {email: "correoEmpresa@hotmail.com"}
-        ]
+// el mutador
+class Cliente {
+    constructor(id, {nombre, apellido, empresa, email}) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.empresa = empresa;
+        this.email = email;
     }
-}};
+}
+
+// DDBB Fake
+const clientesDB = {};
+
+// el resolver
+const root = {
+    cliente: () => {
+        return {
+            "id": 123123123123,
+            "nombre": "Tomy",
+            "apellido": "Munoz",
+            "empresa": "Empresa",
+            "email": "Empresa@empresa.com"
+        }
+    },
+    crearCliente: ({input}) => {
+        const id = require("crypto").randomBytes(10).toString("hex");
+        clientesDB[id] = input;
+        return new Cliente(id, input);
+    }
+};
 
 app.use('/graphql', graphqlHttp({
     // que schema va a utilizar
