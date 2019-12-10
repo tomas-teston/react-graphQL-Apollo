00f254c8394532f5b0880ebc9454df0c78cbd026
Tomás Muñoz Testón <tomini18@hotmail.com>
2019-12-10 20:26:38 +0100

    refactorización de resolvers.

--- index.js
+++ index.js
@@ -2,44 +2,16 @@ import express from 'express';
 // graphql
 import graphqlHttp from 'express-graphql';
 import schema from './schema';
+// resolvers
+import resolvers from './resolvers';
+
+const root = resolvers;
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('todo listo');
 });
-
-// el mutador
-class Cliente {
-    constructor(id, {nombre, apellido, empresa, email}) {
-        this.id = id;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.empresa = empresa;
-        this.email = email;
-    }
-}
-
-// DDBB Fake
-const clientesDB = {};
-
-// el resolver
-const root = {
-    cliente: () => {
-        return {
-            "id": 123123123123,
-            "nombre": "Tomy",
-            "apellido": "Munoz",
-            "empresa": "Empresa",
-            "email": "Empresa@empresa.com"
-        }
-    },
-    crearCliente: ({input}) => {
-        const id = require("crypto").randomBytes(10).toString("hex");
-        clientesDB[id] = input;
-        return new Cliente(id, input);
-    }
-};
 
 app.use('/graphql', graphqlHttp({
     // que schema va a utilizar
---
+++ resolvers.js
@@ -0,0 +1,27 @@
+// el mutador
+class Cliente {
+    constructor(id, {nombre, apellido, empresa, email}) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.empresa = empresa;
+        this.email = email;
+    }
+}
+
+// DDBB Fake
+const clientesDB = {};
+
+// el resolver
+const resolvers = {
+    getCliente: ({id}) => {
+        return new Cliente(id, clientesDB[id]);
+    },
+    crearCliente: ({input}) => {
+        const id = require("crypto").randomBytes(10).toString("hex");
+        clientesDB[id] = input;
+        return new Cliente(id, input);
+    }
+};
+
+export default resolvers;
