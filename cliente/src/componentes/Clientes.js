9f1b9d59e64c9f1109529d12e33f728c5512eb6f
Tomás <tomini18@hotmail.com>
2020-04-14 20:50:50 +0200

    nuevo cliente terminado

--- cliente/src/componentes/Clientes.js
+++ cliente/src/componentes/Clientes.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Query } from 'react-apollo';
 import { CLIENTES_QUERY } from '../queries';
+import { Link } from 'react-router-dom';
 
 export const Clientes = () => (
-    <Query query={CLIENTES_QUERY} >
-        {({ loading, error, data }) => {
+    <Query query={CLIENTES_QUERY} pollInterval={1000}>
+        {({ loading, error, data, startPolling, stopPolling }) => {
             if (loading) return "Cargando...";
             if (error) return `Error: ${error.message}`;
             
@@ -12,7 +13,7 @@ export const Clientes = () => (
 
             return (
                 <>
-                    <h2 className="text-center mt-4">Listado de clientes</h2>
+                    <h2 className="text-center">Listado de clientes</h2>
                     <ul className="list-group mt-4">
                         {data.getClientes.map(cliente => (
                             <li key={cliente.id} className="list-group-item">
@@ -21,9 +22,9 @@ export const Clientes = () => (
                                         {cliente.nombre} {cliente.apellido} - {cliente.empresa}
                                     </div>
                                     <div className="col-md-4 d-flex justify-content-end">
-                                        <a className="btn btn-success d-block d-nd-inline-block" >
+                                        <Link to={`/cliente/editar/${cliente.id}`} className="btn btn-success d-block d-nd-inline-block" >
                                             Editar cliente
-                                        </a>
+                                        </Link>
                                     </div>
                                 </div>
                             </li>
--- cliente/src/componentes/NuevoCliente.js
+++ cliente/src/componentes/NuevoCliente.js
@@ -1,7 +1,155 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import { NUEVO_CLIENTE } from '../mutations';
+import { Mutation } from 'react-apollo';
+import { useHistory } from 'react-router-dom';
 
 export const NuevoCliente = () => {
+
+    const [cliente, setCliente] = useState({
+        nombre: "",
+        apellido: "",
+        empresa: "",
+        edad: "",
+        tipo: ""
+    });
+
+    const history = useHistory();
+
+    const [emails, setEmails] = useState([]);
+
+    const nuevoCampo = () => {
+        setEmails(emails.concat([{email: ""}]));
+    }
+
+    const quitarCampo = index => () => {
+        setEmails(emails.filter((_x, i)=> i !== index));
+    }
+
+    const leerCampo = index => event => {
+        const nuevosEmails = emails.map((x, i) => {
+            if (i !== index) return x;
+            return {
+                ...x,
+                email: event.target.value
+            }
+        })
+        setEmails(nuevosEmails);
+    }
+
+    const handleGoHome = () => {
+        history.push("/");
+    }
+
     return (
-        <h2 className="text-center">Nuevo Cliente</h2>
+        <>
+            <h2 className="text-center">Nuevo Cliente</h2>
+            <div className="row justify-content-center">
+                <Mutation 
+                    mutation={NUEVO_CLIENTE} 
+                    onCompleted={() => handleGoHome()}
+                >
+                { crearCliente => (
+                    <form className="col-md-8 m-3" onSubmit={e => {
+                        e.preventDefault();
+
+                        const input = {
+                            ...cliente,
+                            edad: Number(cliente.edad),
+                            emails: emails
+                        }
+
+                        crearCliente({
+                            variables: {input}
+                        });
+                    }}>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <label>Nombre</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Nombre" 
+                                    onChange={(e) => setCliente({
+                                        ...cliente,
+                                        nombre: e.target.value
+                                    })}
+                                />
+                            </div>
+                            <div className="form-group col-md-6">
+                                <label>Apellido</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Apellido"
+                                    onChange={(e) => setCliente({
+                                        ...cliente,
+                                        apellido: e.target.value
+                                    })}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-md-12">
+                                <label>Empresa</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Empresa"
+                                    onChange={(e) => setCliente({
+                                        ...cliente,
+                                        empresa: e.target.value
+                                    })}
+                                />
+                            </div>
+                            {emails.map((x, index) => (
+                                <div key={index} className="form-group col-md-12">
+                                    <label>Correo: {index + 1}</label>
+                                    <div className="input-group">
+                                        <input type="email" placeholder="email" className="form-control" onChange={leerCampo(index)} value={x.email}/>
+                                        <div className="input-group-append">
+                                            <button type="button" className="btn btn-danger" onClick={quitarCampo(index)}> &times; Eliminar </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                            <div className="form-group d-flex justify-content-center col-md-12">
+                                <button type="button" className="btn btn-warning" onClick={nuevoCampo} >Agregar email</button>
+                            </div>
+                        </div>
+                        <div className="form-row">
+                            <div className="form-group col-md-6">
+                                <label>Edad</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    placeholder="Edad"
+                                    onChange={(e) => setCliente({
+                                        ...cliente,
+                                        edad: e.target.value
+                                    })}
+                                />
+                            </div>
+                            <div className="form-group col-md-6">
+                                <label>Tipo Cliente</label>  
+                                <select 
+                                    className="form-control"
+                                    onChange={(e) => setCliente({
+                                        ...cliente,
+                                        tipo: e.target.value
+                                    })}
+                                >
+                                    <option value="">Elegir...</option>
+                                    <option value="PREMIUM">PREMIUM</option>
+                                    <option value="BASICO">BÁSICO</option>
+                                </select>
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-success float-right">Guardar Cambios</button>
+                    </form>
+                )}                   
+                </Mutation>
+            </div>
+        </>
     )
 }   
